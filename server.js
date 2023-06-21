5b923a39fb24509c881afd6d276a4a93246d8c0e
cyang419 <>
2023-06-21 19:47:59 -0400

    updates

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const path = require('path');
+const api = require('./routes/index');
 
-// Initialize the app and create a port
+const PORT = process.env.port || 3001;
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-// Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use('/api',api)
 
-// Start the server on the port
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
+// get method for index route
+app.get('/',(req,res) =>
+res.sendFile(path.join(__dirname, './public/index.html')));
+
+// get method for /notes
+app.get('/notes',(req,res) =>
+res.sendFile(path.join(__dirname, './public/notes.html')));
+
+// the wildcard has to be last 
+app.get('/*',(req,res) =>
+res.sendFile(path.join(__dirname, './public/index.html')));
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
